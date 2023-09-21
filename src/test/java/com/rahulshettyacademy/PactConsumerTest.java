17a1e796ab43f8e43fe43589ecd78548593b2127
Kiranmoy <paul.kiranmoy@gmail.com>
2023-09-21 16:01:24 +0530

    sucessful run with pactflow

--- src/test/java/com/rahulshettyacademy/PactConsumerTest.java
+++ src/test/java/com/rahulshettyacademy/PactConsumerTest.java
@@ -3,6 +3,7 @@ package com.rahulshettyacademy;
 
 import au.com.dius.pact.consumer.MockServer;
 import au.com.dius.pact.consumer.dsl.PactDslJsonArray;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
@@ -13,6 +14,7 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rahulshettyacademy.controller.LibraryController;
 import com.rahulshettyacademy.model.ProductsPrices;
+import com.rahulshettyacademy.model.SpecificProduct;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -29,8 +31,8 @@ public class PactConsumerTest {
 
   @Pact(consumer = "BooksCatalogue")
   public RequestResponsePact PactallCoursesDetailsPriceCheck(PactDslWithProvider builder) {
-    return builder.given("courses exist")
-      .uponReceiving("getting all courses details")
+    return builder.given("courses exist") // provider state name
+      .uponReceiving("getting all courses details")// interaction name or description
       .path("/allCourseDetails")
       .willRespondWith()
       .status(200)
@@ -49,6 +51,32 @@ public class PactConsumerTest {
     ObjectMapper obj = new ObjectMapper();
     String jsonActual = obj.writeValueAsString(productsPrices);
 
+    Assertions.assertEquals(expectedJson, jsonActual);
+  }
+
+  @Pact(consumer = "BooksCatalogue")
+  public RequestResponsePact getAppiumCourseByName(PactDslWithProvider builder){
+    return builder
+      .given("appium course exists") // provider state name
+      .uponReceiving("get appium course by name") // interaction name or description
+      .path("/getCourseByName/Appium")
+      .willRespondWith()
+      .status(200)
+      .body(
+       new PactDslJsonBody()
+         .integerType("price",44)
+         .stringType("category","mobile")
+      ).toPact();
+  }
+
+  @Test
+  @PactTestFor(pactMethod = "getAppiumCourseByName", port = "9999")
+  public void getProductDetailsByName(MockServer mockServer) throws JsonProcessingException {
+    libraryController.setCoursesBaseUrl(mockServer.getUrl());
+    String expectedJson = "{\"product\":{\"book_name\":\"Appium\",\"id\":\"ttefs36\",\"isbn\":\"ttefs\",\"aisle\":36,\"author\":\"Shetty\"},\"price\":44,\"category\":\"mobile\"}";
+    SpecificProduct specificProduct = libraryController.getProductFullDetails("Appium");
+    ObjectMapper obj = new ObjectMapper();
+    String jsonActual = obj.writeValueAsString(specificProduct);
     Assertions.assertEquals(expectedJson, jsonActual);
   }
 
